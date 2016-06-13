ee6ab8e04d590abfff7cfd74ff5385afe5c422a2
Yaroslav <khaidayev@gmail.com>
2016-06-13 13:09:59 +0600

    Update main.c
    warnings

--- main.c
+++ main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "input.h"
-#include "io.h"
 int main()
 {
     char file_name[256];
-    char strdata[256][256];
+    char strdata[256];
     printf("NameFILE: ");
     scanf("%s",file_name);
 
